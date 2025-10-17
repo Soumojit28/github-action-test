906842bb595a75f957bdf9aa163b7947f5ebb342
Soumojit Ash <soumojitash@gmail.com>
2025-10-18 02:43:44 +0530

    Refactor environment variable access in main.py to use getenv for safety

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from pyfiglet import Figlet
 from os import getenv, environ
 
 
-message = environ["MESSAGE"]
+# In GitHub Actions, environment variables from `action.yml` inputs are accessed via `os.environ`
+# and inputs are automatically exported as env vars. You should use `getenv` for safety.
+message = getenv('MESSAGE', 'Hello, World!')
 f = Figlet(font='slant')
 print(f.renderText(message))
 
